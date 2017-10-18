3ea1888fedf8750365c8599689bc38dcbb4a1376
jdellert <johannes.dellert@student.uni-tuebingen.de>
2017-10-18 18:10:54 +0200

    Steamlined implementation of PMI scores, no solution for smoothing yet.

--- src/de/jdellert/iwsa/ConceptLevelWeightedEditDistanceOutput.java
+++ src/de/jdellert/iwsa/ConceptLevelWeightedEditDistanceOutput.java
@@ -1,6 +1,7 @@
 package de.jdellert.iwsa;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 import de.jdellert.iwsa.align.LevenshteinAlignmentAlgorithm;
@@ -18,6 +19,8 @@ public class ConceptLevelWeightedEditDistanceOutput {
 			int numPairs = 0;
 			int numCognatePairs = 0;
 			int[] cognatePairCorrespondenceCounts = new int[symbolTable.getSize() * symbolTable.getSize()];
+			Arrays.fill(cognatePairCorrespondenceCounts, 1);
+			double cognatePairCorrespondences = symbolTable.getSize() * symbolTable.getSize();
 			System.err.print("Stage 1: Inference of global segment similarity matrix\n");
 			System.err.print("  Step 1: Finding ED-based cognate candidates ...");
 			for (int conceptID = 0; conceptID < database.getNumConcepts(); conceptID++) {
@@ -45,6 +48,8 @@ public class ConceptLevelWeightedEditDistanceOutput {
 			System.err.print(" done. Aligned " + numPairs + " form pairs, of which " + numCognatePairs
 					+ " look like cognates (normalized edit distance < 0.35)\n");
 			int[] randomPairCorrespondenceCounts = new int[symbolTable.getSize() * symbolTable.getSize()];
+			Arrays.fill(randomPairCorrespondenceCounts, 100);
+			double randomPairCorrespondences = symbolTable.getSize() * symbolTable.getSize() * 100;
 			System.err.print("          Creating " + (numCognatePairs * 100)
 					+ " random alignments to model the distribution in absence of correspondences ...");
 			for (int i = 0; i < numCognatePairs * 100; i++) {
@@ -54,6 +59,7 @@ public class ConceptLevelWeightedEditDistanceOutput {
 
 				for (int pos = 0; pos < alignment.getLength(); pos++) {
 					randomPairCorrespondenceCounts[alignment.getSymbolPairIDAtPos(pos, symbolTable)]++;
+					randomPairCorrespondences++;
 				}
 			}
 			System.err.print(" done.");
@@ -61,8 +67,11 @@ public class ConceptLevelWeightedEditDistanceOutput {
 			System.err.print("          Comparing the distributions of symbol pairs for PMI scores ...");
 			for (int symbolPairID = 0; symbolPairID < symbolTable.getSize() * symbolTable.getSize(); symbolPairID++)
 			{
-				double pmiScore = - Math.log((cognatePairCorrespondenceCounts[symbolPairID] + 1.0)/(randomPairCorrespondenceCounts[symbolPairID] / 100.0 + 1));
-				System.err.println(symbolTable.toSymbol(symbolPairID / symbolTable.getSize()) + "\t" + symbolTable.toSymbol(symbolPairID % symbolTable.getSize()) + "\t" + pmiScore);
+				double cognateSymbolPairProbability = cognatePairCorrespondenceCounts[symbolPairID] / cognatePairCorrespondences;
+				double randomSymbolPairProbability = randomPairCorrespondenceCounts[symbolPairID] / randomPairCorrespondences;
+				double pmiScore = Math.log(cognateSymbolPairProbability/randomSymbolPairProbability);
+				//if (cognatePairCorrespondenceCounts[symbolPairID] == 1 || pmiScore < 0.0) continue;
+				System.err.println(symbolTable.toSymbol(symbolPairID / symbolTable.getSize()) + "\t" + symbolTable.toSymbol(symbolPairID % symbolTable.getSize()) + "\t" + cognateSymbolPairProbability + "\t" + randomSymbolPairProbability + "\t" + pmiScore);
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
