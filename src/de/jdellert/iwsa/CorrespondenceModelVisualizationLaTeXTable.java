11a89be89ead4ed9e4ec2e30f69a2a9a63832b48
jdellert <johannes.dellert@uni-tuebingen.de>
2017-11-07 18:45:58 +0100

    Added stubs for planned executables.

---
+++ src/de/jdellert/iwsa/CorrespondenceModelVisualizationLaTeXTable.java
@@ -0,0 +1,17 @@
+package de.jdellert.iwsa;
+
+/**
+ * A program to output the correspondences encoded in a correspondence file as a table
+ * with lines visualizing all associations stronger than a PMI threshold given as a parameter.
+ * 
+ * The result is LaTeX code which can be pasted into any LaTeX document where the font
+ * for the table defines the unicode symbols used by the PhoneticSymbolTable included in the
+ * correspondence file, and needs to be compiled using XeTeX if IPA symbols are used.
+ * 
+ * @author jdellert
+ *
+ */
+
+public class CorrespondenceModelVisualizationLaTeXTable {
+
+}
---
+++ src/de/jdellert/iwsa/InformationWeightOutput.java
@@ -0,0 +1,5 @@
+package de.jdellert.iwsa;
+
+public class InformationWeightOutput {
+
+}
