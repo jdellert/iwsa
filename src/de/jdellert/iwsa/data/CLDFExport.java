10bd134306132b724058bf9246f53095602fee52
jdellert <johannes.dellert@uni-tuebingen.de>
2018-06-07 19:39:09 +0200

    Added functionality for IWDSC-based clustering.

--- src/de/jdellert/iwsa/data/CLDFExport.java
+++ src/de/jdellert/iwsa/data/CLDFExport.java
@@ -1,5 +1,25 @@
 package de.jdellert.iwsa.data;
 
-public class CLDFExport {
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class CLDFExport {
+	
+	public static void exportToFile(LexicalDatabase database, String fileName, boolean header) throws IOException {
+		FileWriter out = new FileWriter(fileName);
+		for (int formID = 0; formID < database.getNumForms(); formID++)
+		{
+			String conceptForForm = database.getConceptNameForForm(formID);
+			String languageForForm = database.getLanguageCodeForForm(formID);
+			
+			String orthForm = database.getAnnotation("Word_Form", formID);
+			String ipaForm = database.getForm(formID).toString(database.getSymbolTable());
+			
+			int cognateSetID = database.getCognateSetID(formID);
+			
+			out.write(conceptForForm + "\t" + languageForForm + "\t" + orthForm + "\t" + ipaForm + "\t" + cognateSetID + "\n");
+		}
+		out.close();
+	}
+	
 }
