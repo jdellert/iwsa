5bf86716bcb4e3b342dc524b5eee72038a3937f3
Arne Rubehn <arne@rubehn.com>
2021-07-02 00:37:12 +0200

    began training DecisionTreeModel for sound generalizations

--- src/main/java/de/jdellert/iwsa/corrmodel/train/GeneralizedCorrModelTrainer.java
+++ src/main/java/de/jdellert/iwsa/corrmodel/train/GeneralizedCorrModelTrainer.java
@@ -1,5 +1,6 @@
 package de.jdellert.iwsa.corrmodel.train;
 
+
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -19,20 +20,32 @@ import org.apache.spark.mllib.util.MLUtils;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.mllib.linalg.DenseVector;
 import scala.Tuple2;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.zip.DataFormatException;
 
 public class GeneralizedCorrModelTrainer {
-    public GeneralizedCorrModelTrainer() {}
+    IPAFeatureTable featureTable;
+    String pathToTrainingData;
+    String pathToLibSVMData;
+    SparkConf sparkConf;
+    JavaSparkContext jsc;
 
-    public void createLIBSVMData(IPAFeatureTable featureTable, String inputFilepath, String outputFilepath,
-                                 int lowerPMIFilterBoundary, int upperPMIFilterBoundary) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(inputFilepath));
-        Writer writer = new FileWriter(outputFilepath);
+    public GeneralizedCorrModelTrainer(IPAFeatureTable featureTable, String pathToTrainingData, String pathToLibSVMData) {
+        this.featureTable = featureTable;
+        this.pathToTrainingData = pathToTrainingData;
+        this.pathToLibSVMData = pathToLibSVMData;
+        sparkConf = new SparkConf().setAppName("FeatureGeneralizationsDecisionTree").setMaster("local[2]");
+        jsc = new JavaSparkContext(sparkConf);
+    }
+
+    public void createLibSVMData(int lowerPMIFilterBoundary, int upperPMIFilterBoundary) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(pathToTrainingData));
+        Writer writer = new FileWriter(pathToLibSVMData);
         while (br.readLine() != null) {
             String[] fields = br.readLine().split("\t");
             double score = Double.parseDouble(fields[2]);
@@ -40,17 +53,12 @@ public class GeneralizedCorrModelTrainer {
                 continue;
             }
             writer.write(String.valueOf(score));
-            int[] sound1Features = featureTable.get(fields[0]);
-            int[] sound2Features = featureTable.get(fields[1]);
 
-            if (sound1Features == null || sound2Features == null) {
+            int[] soundPairFeatures = featureTable.getPair(fields[0], fields[1]);
+
+            if (soundPairFeatures == null) {
                 continue;
             }
-            int[] soundPairFeatures = new int[sound1Features.length * 2];
-
-            // concatenate feature arrays
-            System.arraycopy(sound1Features, 0, soundPairFeatures, 0, sound1Features.length);
-            System.arraycopy(sound2Features, 0, soundPairFeatures, sound1Features.length, sound2Features.length);
 
             for (int i = 0; i < soundPairFeatures.length; i++) {
                 if (soundPairFeatures[i] != 0) {
@@ -64,10 +72,12 @@ public class GeneralizedCorrModelTrainer {
         writer.close();
     }
 
-    public void train(String pathToLIBSVMFile) {
-        SparkConf sparkConf = new SparkConf().setAppName("FeatureGeneralizationsDecisionTree").setMaster("local[2]");
-        JavaSparkContext jsc = new JavaSparkContext(sparkConf);
-        JavaRDD<LabeledPoint> data = MLUtils.loadLibSVMFile(jsc.sc(), pathToLIBSVMFile).toJavaRDD();
+    public DecisionTreeModel train() {
+        return train(null);
+    }
+
+    public DecisionTreeModel train(String pathToSaveFile) {
+        JavaRDD<LabeledPoint> data = MLUtils.loadLibSVMFile(jsc.sc(), pathToLibSVMData).toJavaRDD();
         JavaRDD<LabeledPoint>[] splits = data.randomSplit(new double[]{0.8, 0.2});
         JavaRDD<LabeledPoint> trainingData = splits[0];
         JavaRDD<LabeledPoint> testData = splits[1];
@@ -89,31 +99,70 @@ public class GeneralizedCorrModelTrainer {
         System.out.println("Test Mean Squared Error: " + testMSE);
         System.out.println("Learned regression tree model:\n" + model.toDebugString());
 
-        // Save and load model
-        model.save(jsc.sc(), "src/main/java/de/jdellert/iwsa/corrmodel/train/tmp/DecisionTreeModel");
+        if (pathToSaveFile != null) {
+            model.save(jsc.sc(), pathToSaveFile);
+        }
+
+        return model;
     }
 
-    public DecisionTreeModel loadModel(String filePath, JavaSparkContext jsc) {
+    public DecisionTreeModel loadModel(String filePath) {
         return DecisionTreeModel.load(jsc.sc(), filePath);
     }
 
 
     public static void main(String[] args) {
+        String pathToTrainingData = "src/main/java/de/jdellert/iwsa/corrmodel/train/global-corr-model-output.tsv";
+        String pathToLibSVMData = "src/main/java/de/jdellert/iwsa/corrmodel/train/libsvm-corr-data.txt";
         try {
             IPAFeatureTable featureTable = new IPAFeatureTable("src/main/java/de/jdellert/iwsa/corrmodel/train/all_ipa_symbols.csv");
-            GeneralizedCorrModelTrainer trainer = new GeneralizedCorrModelTrainer();
-            //trainer.createLIBSVMData(featureTable, "src/main/java/de/jdellert/iwsa/corrmodel/train/global-corr-model-output.tsv",
-              //      "src/main/java/de/jdellert/iwsa/corrmodel/train/libsvm-corr-data.txt", -1, 1);
-            trainer.train("src/main/java/de/jdellert/iwsa/corrmodel/train/libsvm-corr-data.txt");
+            GeneralizedCorrModelTrainer trainer = new GeneralizedCorrModelTrainer(featureTable, pathToTrainingData, pathToLibSVMData);
+            //trainer.createLibSVMData(-1, 1);
+            //DecisionTreeModel model = trainer.train("src/main/java/de/jdellert/iwsa/corrmodel/train/model");
+            DecisionTreeModel model = trainer.loadModel("src/main/java/de/jdellert/iwsa/corrmodel/train/model");
 
-            // TODO train decision tree on LIBSVM data
-            //SparkSession spark = SparkSession.builder().config(sc.getConf()).enableHiveSupport().getOrCreate();
-            //Dataset<Row> people = spark.read().parquet("...").as(Encoders.bean(Person.class));
+            Writer wr = new FileWriter("src/main/java/de/jdellert/iwsa/corrmodel/train/inferred_scores_s.tsv");
+            List<String> ipaKeyList = new ArrayList<>(featureTable.getFeatureTable().keySet());
+            /*Map<String, int[]> filteredMap = featureTable.getFeatureTable().entrySet()
+                    .stream()
+                    .filter(map -> map.getValue()[2] > -1)
+                    .collect(Collectors.toMap(map -> map.getKey(), map -> map.getValue()));
+            List<String> ipaKeyList = new ArrayList<>(filteredMap.keySet());*/
+            int writtenPairs = 0;
+            for (int i = 0; i < ipaKeyList.size(); i++) {
+                String sound1 = "s";
+                String sound2 = ipaKeyList.get(i);
+                int[] featuresAsInt = featureTable.getPair(sound1, sound2);
+                double[] features = Arrays.stream(featuresAsInt).asDoubleStream().toArray();
+                DenseVector featureVector = new DenseVector(features);
+                double score = model.predict(featureVector);
+                wr.write(sound1 + "\t" + sound2 + "\t" + score + "\n");
+            }
+
+            wr.close();
+
+            // TEST
+            /*Map<String, int[]> filteredMap = featureTable.getFeatureTable().entrySet()
+                    .stream()
+                    .filter(map -> map.getValue()[2] > -1)
+                    .collect(Collectors.toMap(map -> map.getKey(), map -> map.getValue()));
+            List<String> ipaKeyList = new ArrayList<>(filteredMap.keySet());
+            for (int i = 0; i < 100; i++) {
+                String sound1 = ipaKeyList.get((int) (Math.random() * ipaKeyList.size()));
+                String sound2 = ipaKeyList.get((int) (Math.random() * ipaKeyList.size()));
+                int[] featuresAsInt = featureTable.getPair(sound1, sound2);
+                double[] features = Arrays.stream(featuresAsInt).asDoubleStream().toArray();
+                DenseVector testVector = new DenseVector(features);
+                System.out.println("Sound correspondence: " + sound1 + " - " + sound2 + "   SCORE: " + model.predict(testVector));
+            }*/
+
 
         } catch (IOException e) {
             e.printStackTrace();
         } catch (DataFormatException e) {
             e.printStackTrace();
         }
+
+
     }
 }
--- src/main/java/de/jdellert/iwsa/corrmodel/train/IPAFeatureTable.java
+++ src/main/java/de/jdellert/iwsa/corrmodel/train/IPAFeatureTable.java
@@ -20,12 +20,26 @@ public class IPAFeatureTable {
             int[] features = new int[fields.length - 1];
             for (int i = 0; i < features.length; i++) {
                 String feature = fields[i+1];
-                int featureValue = switch (feature) {
+                /*int featureValue = switch (feature) {
                     case "+" -> 1;
                     case "-" -> -1;
                     case "0" -> 0;
                     default -> throw new DataFormatException();
-                };
+                };*/
+                int featureValue;
+                switch (feature) {
+                    case "+":
+                        featureValue = 1;
+                        break;
+                    case "0":
+                        featureValue = 0;
+                        break;
+                    case "-":
+                        featureValue = -1;
+                        break;
+                    default:
+                        throw new DataFormatException();
+                }
                 features[i] = featureValue;
             }
             featureTable.put(fields[0], features);
@@ -35,6 +49,23 @@ public class IPAFeatureTable {
 
     public int[] get(String key) {
         return featureTable.get(key);
+    }
+
+    public int[] getPair(String sound1, String sound2) {
+        int[] sound1Features = featureTable.get(sound1);
+        int[] sound2Features = featureTable.get(sound2);
+
+        if (sound1Features == null || sound2Features == null) {
+            return null;
+        }
+
+        int[] soundPairFeatures = new int[sound1Features.length * 2];
+
+        // concatenate feature arrays
+        System.arraycopy(sound1Features, 0, soundPairFeatures, 0, sound1Features.length);
+        System.arraycopy(sound2Features, 0, soundPairFeatures, sound1Features.length, sound2Features.length);
+
+        return soundPairFeatures;
     }
 
     public Map<String, int[]> getFeatureTable() {
